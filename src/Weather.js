55f519b2d1566f2ebf3c2d7c87937ff2d731466b
ozgeosbourne <ozgenurayyasar@gmail.com>
2023-01-06 17:26:05 +0300

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Istanbul",
-    temperature: 16,
-    date: "Sunday 12:22",
-    description: "Cloudy",
-    humidity: 80,
-    wind: 10
-  };
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      icon: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+      description: response.data.weather[0].description,
+      date: "Friday, 17:17"
 
-  return (
+    });
+    
+    setReady(true);
+
+  }
+  if (ready) {
+    return (
     <div className="weather-app">
       <form className="search-form" id="search-form">
         <div className="row">
@@ -22,6 +32,7 @@ export default function Weather() {
               placeholder="Type a city..."
               className="form-control"
               id="search-input"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -32,7 +43,7 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <button className="btn btn-success" id="current-button">
+            <button className="btn btn-success w-100" id="current-button">
               Current
             </button>
           </div>
@@ -40,22 +51,22 @@ export default function Weather() {
       </form>
       <h1 id="city">{weatherData.city}</h1>
       <ul>
-        <li id="current-date">{weatherData.date}</li>
-        <li id="forecast">{weatherData.description}</li>
+        <li>{weatherData.date}</li>
+        <li className="text-capitalize">{weatherData.description}</li>
       </ul>
       <div className="row">
         <div className="col-6">
           <div className="clearfix">
             <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy icon"
+              src={weatherData.icon}
+              alt={weatherData.description}
               className="weather-icon float-left"
             />
             <div className="float-left">
               <span className="temperature float-left" id="temperature">
-                {weatherData.temperature}
+                {Math.round(weatherData.temperature)}
               </span>
-              <span class="units">
+              <span className="units">
                 <a href="/" id="celcius">
                   °C
                 </a>{" "}
@@ -69,11 +80,20 @@ export default function Weather() {
         </div>
         <div className="col-6">
           <ul className="humidity">
-            <li id="humidity">Humidity: {weatherData.humidity}%</li>
-            <li id="wind">Wind: {weatherData.wind}km/h</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}km/h</li>
           </ul>
         </div>
       </div>
     </div>
   );
+  } else {
+  const apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let city = "Istanbul";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+  
 }
